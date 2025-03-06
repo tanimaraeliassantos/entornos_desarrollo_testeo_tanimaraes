52ab8bdb51b90e1427faa291a5836ca3141fce40
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-03-06 20:14:14 +0100

    Creados archivos

--- src/main/java/Libro.java
+++ src/main/java/Libro.java
@@ -1,41 +1,86 @@
+/**
+ * Esta clase representa un libro con título, autor y año de publicación.
+ */
 public class Libro {
 
-    // TODO: Documentar estos atributos
+    /**
+     * El título del libro.
+     */
     private String titulo;
+
+    /**
+     * El autor del libro.
+     */
     private String autor;
+
+    /**
+     * El año de publicación del libro.
+     */
     private int anioPublicacion;
 
-
-    // TODO: Documentar este método
-    // TODO: Testear este método
+    /**
+     * Constructor de la clase Libro.
+     *
+     * @param titulo         El título del libro.
+     * @param autor          El autor del libro.
+     * @param anioPublicacion El año de publicación del libro.
+     */
     public Libro(String titulo, String autor, int anioPublicacion) {
         this.titulo = titulo;
         this.autor = autor;
         this.anioPublicacion = anioPublicacion;
     }
 
+    /**
+     * Obtiene el título del libro.
+     *
+     * @return El título del libro.
+     */
     public String getTitulo() {
         return titulo;
     }
 
+    /**
+     * Establece el título del libro.
+     *
+     * @param titulo El nuevo título del libro.
+     */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
-    // TODO: Documentar este método
+    /**
+     * Obtiene el autor del libro.
+     *
+     * @return El autor del libro.
+     */
     public String getAutor() {
         return autor;
     }
 
+    /**
+     * Establece el autor del libro.
+     *
+     * @param autor El nuevo autor del libro.
+     */
     public void setAutor(String autor) {
         this.autor = autor;
     }
 
-    // TODO: Documentar este método
+    /**
+     * Obtiene el año de publicación del libro.
+     *
+     * @return El año de publicación del libro.
+     */
     public int getAnioPublicacion() {
         return anioPublicacion;
     }
 
+    /**
+     * Establece el año de publicación del libro.
+     *
+     * @param anioPublicacion El nuevo año de publicación del libro.
+     */
     public void setAnioPublicacion(int anioPublicacion) {
         this.anioPublicacion = anioPublicacion;
     }
